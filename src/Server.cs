63e4cad1d55029c476fc6f3a0c7e9cc77bc2aca7
Scott Zonneveldt <scott.zonneveldt@howatsonco.com.au>
2024-05-11 13:48:23 +1000

    pass stage

--- src/Server.cs
+++ src/Server.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -7,18 +8,34 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-Socket client = server.AcceptSocket(); // wait for clien  // Send a response to the client.
+Socket client = server.AcceptSocket(); // wait for client
 
-string responseString = "HTTP/1.1 200 OK\r\n\r\n";
-byte[] data = System.Text.Encoding.ASCII.GetBytes(responseString);
+NetworkStream stream = new NetworkStream(client);
+StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+StreamWriter writer = new StreamWriter(stream, Encoding.UTF8);
 
-// Get a network stream from the accepted client.
-using (NetworkStream stream = new NetworkStream(client))
+
+string? requestLine = reader.ReadLine();
+Console.WriteLine("request recieved2 {0}", requestLine);
+
+var lineSplit = requestLine.Split(" ");
+string method = lineSplit[0];
+string path = lineSplit[1];
+string httpVersion = lineSplit[2];
+
+string responseString = "HTTP/1.1 404 Not Found\r\n\r\n";
+if (path == "/")
 {
-    // Write the response data to the network stream.
-    stream.Write(data, 0, data.Length);
-    Console.WriteLine("Sent: {0}", responseString);
+    responseString = "HTTP/1.1 200 OK\r\n\r\n";
+
 }
+
+
+byte[] data = Encoding.ASCII.GetBytes(responseString);
+
+// Write the response data to the network stream.
+stream.Write(data, 0, data.Length);
+Console.WriteLine("Sent: {0}", responseString);
 
 // Close the connection to the client.
 client.Close();
