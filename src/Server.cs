2f731e7d25ad5a433f1d02f7a8bc2652e85608f5
Scott Zonneveldt <scott.zonneveldt@howatsonco.com.au>
2024-05-11 13:11:58 +1000

    pass stage

--- src/Server.cs
+++ src/Server.cs
@@ -7,4 +7,18 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+Socket client = server.AcceptSocket(); // wait for clien  // Send a response to the client.
+
+string responseString = "HTTP/1.1 200 OK\r\n\r\n";
+byte[] data = System.Text.Encoding.ASCII.GetBytes(responseString);
+
+// Get a network stream from the accepted client.
+using (NetworkStream stream = new NetworkStream(client))
+{
+    // Write the response data to the network stream.
+    stream.Write(data, 0, data.Length);
+    Console.WriteLine("Sent: {0}", responseString);
+}
+
+// Close the connection to the client.
+client.Close();
